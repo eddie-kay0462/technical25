6461dffd32b6f8bbf54e8624678ce354f5ce180f
Edward Ofosu Mensah <edward.mensah@ashesi.edu.gh>
2025-06-05 16:51:39 +0000

    Brief revision of arrays

---
+++ week1/arrays.py
@@ -0,0 +1,29 @@
+#creating arrays using the array module
+import array
+
+# my_array = array.array('i', [1, 2, 3, 4, 5])
+# print(my_array)
+
+
+#using the numpy module
+import numpy as np
+# my_array = np.array([1, 2, 3, 4, 5])
+
+#find the length of an array in python
+
+import array as arr
+# numbers  = arr.array('i', [1, 2, 3])
+# print(len(numbers))
+
+
+#array indexing and how to access individual items in an array in python
+# numbers  = arr.array('i', [10, 20, 30])
+# print(numbers[-1])
+
+
+#slicing of an array : Syntax arr[start:end:step]
+numbers  = arr.array('i', [10, 20, 30, 40, 50])
+# numbers.remove(30)
+# numbers.pop(1)
+# del numbers[4]
+print(numbers)
