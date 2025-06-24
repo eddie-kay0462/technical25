1d3e18192e8341a085c7e61535dadd4a2f03529a
Edward Ofosu Mensah <edward.mensah@ashesi.edu.gh>
2025-06-24 15:33:30 +0000

    Implementing bubble sort from scratch

--- week.py/bubble_sort.py
+++ week.py/bubble_sort.py
@@ -1,6 +1,5 @@
 def bubble_sort(list):
-    unsorted_until_index  =len(list) -1
-    
+    unsorted_until_index = len(list) -1
     sorted = False
 
     while not sorted:
@@ -9,8 +8,8 @@ def bubble_sort(list):
             if list[i] > list[i+1]:
                 list[i], list[i+1] = list[i+1], list[i]
                 sorted = False
-
     return list
 
 
-print(bubble_sort([4, 5, 8, 2, 3, 1]))
+
+print(bubble_sort([3, 2, 5, 4, 1]))
